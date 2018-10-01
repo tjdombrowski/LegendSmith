4c2f7a62a8fb22e31538d8e5265a36cbb290e19a
tjdombrowski <tjdombrowski@madisoncollege.edu>
2018-10-01 01:20:59 -0500

    Adding the basis of the delete, insert and update methods to the dao.

--- src/main/java/edu/matc/legendsmith/persistence/LegendaryDao.java
+++ src/main/java/edu/matc/legendsmith/persistence/LegendaryDao.java
@@ -6,6 +6,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
@@ -57,6 +58,43 @@ public class LegendaryDao {
         return legendary;
     }
 
+    /**
+     * update user
+     * @param legendary  User to be inserted or updated
+     */
+    public void saveOrUpdate(Legendary legendary) {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        session.saveOrUpdate(legendary);
+        transaction.commit();
+        session.close();
+    }
+
+    /**
+     * update user
+     * @param legendary  Legendary to be inserted or updated
+     */
+    public int insert(Legendary legendary) {
+        int id = 0;
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        id = (int)session.save(legendary);
+        transaction.commit();
+        session.close();
+        return id;
+    }
+
+    /**
+     * Delete a user
+     * @param legendary Legendary to be deleted
+     */
+    public void delete(Legendary legendary) {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        session.delete(legendary);
+        transaction.commit();
+        session.close();
+    }
 
 
 }

ca2ad17a0e446750aceab9f698793ba7e3e1a825
tjdombrowski <tjdombrowski@madisoncollege.edu>
2018-10-04 21:02:47 -0500

    Adding a dao for retrieving primary items and the basis of a test class for the dao.

---
+++ src/test/java/edu/matc/legendsmith/persistence/PrimaryItemDaoTest.java
@@ -0,0 +1,24 @@
+package edu.matc.legendsmith.persistence;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PrimaryItemDaoTest {
+    @Test
+    void getAllPrimaryItems() {
+    }
+
+    @Test
+    void insert() {
+    }
+
+    @Test
+    void saveOrUpdate() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+}

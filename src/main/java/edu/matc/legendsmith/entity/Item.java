8c27c25ac7ceee236c5ad9e258416e55016499ff
tjdombrowski <tjdombrowski@madisoncollege.edu>
2018-11-30 13:17:53 -0600

    Adding fields, getters, setters, toTstring and equals and hashcode for TaskItem.

--- src/main/java/edu/matc/legendsmith/entity/Item.java
+++ src/main/java/edu/matc/legendsmith/entity/Item.java
@@ -1,4 +1,16 @@
 package edu.matc.legendsmith.entity;
 
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Table(name = "Item")
+@Entity(name = "Item")
 public class Item {
+    @Id
+    private int gw2ItemId;
+    private String name;
+
+
+
 }
--- src/main/java/edu/matc/legendsmith/entity/TaskItem.java
+++ src/main/java/edu/matc/legendsmith/entity/TaskItem.java
@@ -1,4 +1,82 @@
 package edu.matc.legendsmith.entity;
 
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+
+@Entity(name = "TaskItem")
+@Table(name = "TaskItem")
 public class TaskItem {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO, generator="native")
+    @GenericGenerator(name = "native",strategy = "native")
+    private int id;
+
+    private int quantity;
+
+    private Task task;
+
+    private Item item;
+
+    public TaskItem() {}
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public Task getTask() {
+        return task;
+    }
+
+    public void setTask(Task task) {
+        this.task = task;
+    }
+
+    public Item getItem() {
+        return item;
+    }
+
+    public void setItem(Item item) {
+        this.item = item;
+    }
+
+    @Override
+    public String toString() {
+        return "TaskItem{" +
+                "id=" + id +
+                ", quantity=" + quantity +
+                ", task=" + task +
+                ", item=" + item +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        TaskItem taskItem = (TaskItem) o;
+
+        if (id != taskItem.id) return false;
+        return quantity == taskItem.quantity;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + quantity;
+        return result;
+    }
 }

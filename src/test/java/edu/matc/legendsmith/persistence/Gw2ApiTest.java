8822827862053d4f6e5e7de6768f228882f60b2d
tjdombrowski <tjdombrowski@madisoncollege.edu>
2018-11-30 13:02:19 -0600

    Adding a test for the gw2 api.

--- src/test/java/edu/matc/legendsmith/persistence/Gw2ApiTest.java
+++ src/test/java/edu/matc/legendsmith/persistence/Gw2ApiTest.java
@@ -1,5 +1,6 @@
 package edu.matc.legendsmith.persistence;
 
+import com.arenanet.gw2.*;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import edu.matc.legendsmith.entity.*;
 import edu.matc.legendsmith.test.util.Database;
@@ -11,12 +12,17 @@ import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 class Gw2ApiTest {
 
+    /**
+     * Tests whether the commerce/prices results are being correctly retrieved and mapped.
+     *
+     * @throws Exception
+     */
     @Test
-    public void testRetrieveGw2ItemResponse() throws Exception {
+    public void testRetrieveGw2CommerceResponse() throws Exception {
         /*
         TODO: I will want data on the following:
         -Elder Wood Planks                  19709
@@ -30,9 +36,7 @@ class Gw2ApiTest {
         -Amalgamated Gemstones              68063
         -T5 ingredients (bones for ?)
             -large bones                    24341
-            -incandescent dust              24276
-            -vial of potent blood           24294
-            -large fangs                    24356
+            -incandescent dust ngs                    24356
             -large claws                    24350
             -intricate totems               24299
             -potent venom sacs              24282
@@ -40,27 +44,22 @@ class Gw2ApiTest {
         -T6 ingredients
         -T4 mats
         -t3 mats
-
-
+        */
 
         Client client = ClientBuilder.newClient();
 
         WebTarget target =
-                client.target("https://api.guildwars2.com/v2/commerce/prices/ID");
+                client.target("https://api.guildwars2.com/v2/commerce/prices/19684");
         String response = target.request(MediaType.APPLICATION_JSON).get(String.class);
 
         ObjectMapper mapper = new ObjectMapper();
-        Results results = mapper.readValue(response, Results.class);
+        Response results = mapper.readValue(response, Response.class);
 
-        Results expectedResults = new Results(424, "Mini Aurene",
-                "https://render.guildwars2.com/file/3359C5F30AE534F513ABF603023194F0E703D2C3/1601469.png",
-                429, 79766);
+        assertEquals(19684, results.getId());
+        assertFalse(results.isWhitelisted());
+        assertNotNull(results.getBuys()); // The response for the buy and sell orders are going to change, so I'm just checking whether or not I'm getting data back
+        assertNotNull(results.getSells());
 
-        assertEquals(424, results.getId());
-        assertEquals("Mini Aurene", results.getName());
-        */
     }
 
-    @Test
-    public void testRetrieveGw2CommerceResponse() {}
 }

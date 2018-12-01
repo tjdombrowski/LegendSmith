a7d96c9c316da8a649bdcc2f4b8d47d67a38ed0d
tjdombrowski <tjdombrowski@madisoncollege.edu>
2018-12-01 16:35:49 -0600

    Creating an entity to represent the cost of an item in gold, silver, and copper.

---
+++ src/main/java/edu/matc/legendsmith/entity/ItemPrice.java
@@ -0,0 +1,19 @@
+package edu.matc.legendsmith.entity;
+
+public class ItemPrice {
+    private int goldPrice;
+    private int silverPrice;
+    private int copperPrice;
+
+    public void ItemPrice() {}
+
+    public void ItemPrice(int sellOrderPrice) {
+
+    }
+
+    private int convertSellPriceToDenominations(int sellOrderPrice) {
+
+
+        return 0;
+    }
+}

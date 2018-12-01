2c0a3c140714d0bdd7f54abcad58942e15e2583e
tjdombrowski <tjdombrowski@madisoncollege.edu>
2018-12-01 17:01:55 -0600

    Creating getter and setters for ItemPrice, as well as a method that sets the values of gold, silver, and copper, based on the given price.

--- src/main/java/edu/matc/legendsmith/entity/ItemPrice.java
+++ src/main/java/edu/matc/legendsmith/entity/ItemPrice.java
@@ -7,13 +7,49 @@ public class ItemPrice {
 
     public void ItemPrice() {}
 
-    public void ItemPrice(int sellOrderPrice) {
-
+    public void ItemPrice(int price) {
+        setDenominationValues(price);
     }
 
-    private int convertSellPriceToDenominations(int sellOrderPrice) {
+    /**
+     * This calculates and sets the object's gold, silver, and copper values from the given price, representing a sum total of all the coins.
+     * Each coins is worth 1 copper, and each copper is worth 100 silver and each gold is worth 100 silver.
+     *
+     * @param price the price in total coins
+     */
+    private void setDenominationValues(int price) {
+        goldPrice = price / 10000; //the result in gold, silver, and copper should be truncated, not rounded
+        price = price - (goldPrice * 10000); //Subtract the gold value of the coins from the remaining price (same with silver)
+
+        silverPrice = price / 100;
+        price = price - (silverPrice * 100);
+
+        copperPrice = price;
+    }
 
 
-        return 0;
+
+    public int getGoldPrice() {
+        return goldPrice;
+    }
+
+    public void setGoldPrice(int goldPrice) {
+        this.goldPrice = goldPrice;
+    }
+
+    public int getSilverPrice() {
+        return silverPrice;
+    }
+
+    public void setSilverPrice(int silverPrice) {
+        this.silverPrice = silverPrice;
+    }
+
+    public int getCopperPrice() {
+        return copperPrice;
+    }
+
+    public void setCopperPrice(int copperPrice) {
+        this.copperPrice = copperPrice;
     }
 }
